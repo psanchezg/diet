053b5100eb8806f978c807a137b6cb95b57fb934
Pablo Sánchez <pablo.sanchez@aranova.es>
2014-11-14 12:00:59 +0100

    Lint and pass test

--- tests/plugins_onload.js
+++ tests/plugins_onload.js
@@ -19,17 +19,19 @@ describe(subject + 'Plain Onload Plugin', function(){
 	});
 	it('should create an app and use the non-regular module `yo` plugin then makes sure that yo.message is "Yo!"'.grey
 	, function(done){
-		var app = new server({debug: false});
+		var app = new server({debug: true});
 		app.domain('http://localhost:9008');
 		var yo = app.plugin('./test_plugins/yo.js');
-		assert.equal(yo.message, 'Yo!');
-		done()
+    app.start(function(){
+      assert.equal(yo.message, 'Yo!');
+      done()
+    });
 	});
 	
 	it('should create an app and use the regular module `test_module` plugin then makes sure that app.test_module returns "ready" after app.start'.grey
 	, function(done){
 		var app = new server({ debug: true });
-		app.domain('http://localhost:9032');
+		app.domain('http://localhost:9033');
 		var test = app.plugin('test_module');
 		app.start(function(){
 			assert.equal(isset(test.global), true);
